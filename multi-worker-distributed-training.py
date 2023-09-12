97ffc33efba36322c78c6c5c1ba0da9139ece5a9
Aniket Mishrikotkar <71699313+aniket-mish@users.noreply.github.com>
2023-09-12 15:00:31 +0530

    Update multi-worker-distributed-training.py

--- multi-worker-distributed-training.py
+++ multi-worker-distributed-training.py
@@ -32,7 +32,6 @@ def decay(epoch):
         return 1e-5
 
 
-# Functional API
 def build_and_compile_cnn_model():
     print("Training CNN model")
     model = tf.keras.Sequential(
@@ -55,7 +54,6 @@ def build_and_compile_cnn_model():
     return model
 
 
-# Sequential API
 def build_and_compile_cnn_model_with_batch_norm():
     print("Training CNN model with batch normalization")
     model = tf.keras.models.Sequential()
@@ -128,6 +126,20 @@ def main(args):
             multi_worker_model = build_and_compile_cnn_model_with_batch_norm()
         elif model_type == "cnn_dropout":
             multi_worker_model = build_and_compile_cnn_model_with_dropout()
+        else:
+            Exception(f"Entered {model_type} is not supported")
+
+    def _preprocess(bytes_inputs):
+        decoded = tf.io.decode_jpeg(bytes_inputs, channels=1)
+        resized = tf.image.resize(decoded, size=(28, 28))
+        return tf.cast(resized, dtype=tf.uint8)
+
+    def _get_serve_image_fn(model):
+        @tf.function(input_signature=[tf.TensorSpec([None], tf.string)])
+        def serve_image_fn(bytes_inputs):
+            decoded_images = tf.map_fn(_preprocess, bytes_inputs, dtype=tf.uint8)
+            return model(decoded_images)
+        return serve_image_fn
 
     # Define the checkpoint directory to store the checkpoints
     checkpoint_dir = args.checkpoint_dir
@@ -165,6 +177,14 @@ def main(args):
         model_path = args.saved_model_dir + "/worker_tmp_" + str(TASK_INDEX)
 
     multi_worker_model.save(model_path)
+
+    signatures = {
+    "serving_default": _get_serve_image_fn(multi_worker_model).get_concrete_function(
+        tf.TensorSpec(shape=[None], dtype=tf.string)
+        )    
+    }
+
+    tf.saved_model.save(multi_worker_model, model_path, signatures=signatures)
 
 
 if __name__ == "__main__":
